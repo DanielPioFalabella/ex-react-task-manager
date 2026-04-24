8e79a5930f21bf2b0131c21a0e590b42969573d8
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-04-24 11:24:23 +0200

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import TaskList from "./pages/TaskList"
 import AddTask from "./pages/AddTask"
+import { GlobalProvider } from "./context/GlobalContaxt"
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList />}/>
-        <Route path="/addTask" element={<AddTask />}/>
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TaskList />}/>
+          <Route path="/addTask" element={<AddTask />}/>
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
     </>
   )
 }
---
+++ src/context/GlobalContaxt.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const GlobalContext = createContext() 
+
+export const GlobalProvider = ({children}) => {
+const [tasks, setTasks] = useState([])
+
+useEffect (() => {
+axios.get(`${import.meta.env.VITE_API_URL}/tasks`)
+.then(res => {
+    setTasks(res.data)
+    console.log(res.data)
+})
+.catch(err => console.error(err))
+}, [])
+
+return (
+    <GlobalContext.Provider value={{tasks}}>
+        {children}
+    </GlobalContext.Provider>
+)
+}
+
+export default GlobalContext
